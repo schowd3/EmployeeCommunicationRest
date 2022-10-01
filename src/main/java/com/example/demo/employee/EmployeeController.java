5039ea735b789ddc9a523b75319084a6ac786626
Siam Chowdhury <51762913+schowd3@users.noreply.github.com>
2022-09-30 21:43:25 -0700

    Update EmployeeController.java

--- src/main/java/com/example/demo/employee/EmployeeController.java
+++ src/main/java/com/example/demo/employee/EmployeeController.java
@@ -13,35 +13,30 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping(path = "api/v1/employee") //local hosting deer
+@RequestMapping(path = "api/v1/employee") 
 public class EmployeeController {
-    // want to use the method inside employeeService - talking to service layer
+    
     private final EmployeeService employeeService;
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;//  EmployeeService gets instantiated and injected to the constructor. - Autowired for rest
-
-        //this.employeeService = employeeService;// doesnt work
-        //this.employeeService = new EmployeeService();// does work but not good design, we should use DI instead
+        this.employeeService = employeeService;
     }
 
-    //getMapping very important
+    
     @GetMapping
     public List<Employee> getEmployee(){
         return employeeService.getEmployee();
     }
-    //employee comes from client
-    //post add new resource
+ 
     @PostMapping
     public void registerNewStudent(@RequestBody Employee employee){
         employeeService.addNewEmployee(employee);
     }
     @DeleteMapping(path = "{employeeId}")
     public void deleteStudent(
-            @PathVariable("employeeId") Long employeeId){//take something from the path.
+            @PathVariable("employeeId") Long employeeId){
         employeeService.deleteEmployee(employeeId);
     }
-    //Using Transactional we do not need to write/implement any JPQL
     @PutMapping(path = "{employeeId}")
     public void updateStudent(
             @PathVariable("employeeId") Long employeeId,
